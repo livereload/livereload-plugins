e2e4edf9bd32fe6c4cb412382921fe1ac115ddf2
Andrey Tarantsov <andrey@tarantsov.com>
2012-07-11 05:05:22 +0700

    Delete ZURB-foundation 2.x to avoid case probs

--- SASS.lrplugin/lib/ZURB-foundation/lib/ZURB-foundation.rb
+++
@@ -1,9 +0,0 @@
-require 'compass'
-Compass::Frameworks.register("ZURB-foundation", :path => File.join(File.dirname(__FILE__), ".."))
-
-module ZURBfoundation
-  
-  VERSION = "2.1.3b"
-  DATE = "2011-12-21"
-
-end
