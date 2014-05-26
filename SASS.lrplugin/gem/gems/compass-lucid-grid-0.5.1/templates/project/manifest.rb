55ed043c3165aad7fab5a6b1e0ca2176204d5574
Andrey Tarantsov <andrey@tarantsov.com>
2014-05-26 14:24:51 +0700

    fixed conflicts (LOL merge -s ours; push -f)

---
+++ SASS.lrplugin/gem/gems/compass-lucid-grid-0.5.1/templates/project/manifest.rb
@@ -0,0 +1,33 @@
+description "A Compass/SASS grid for devs who care about semantics."
+
+discover :all
+
+help %Q{
+
+***
+
+THE LUCID GRID
+plugin by Yifei Zhang [http://yifei.co]
+
+A Compass/SASS grid for developers who love semantics.
+
+A Compass/SASS grid that outputs lean, semantic stylesheets. Avoids CSS repetition on SASS compilation and reduces the need for wrapper <div>s.
+
+***
+
+}
+
+welcome_message %Q{
+
+***
+
+THE LUCID GRID
+plugin by Yifei Zhang [http://yifei.co]
+
+A Compass/SASS grid that outputs lean, semantic stylesheets. Avoids CSS repetition on SASS compilation and reduces the need for wrapper <div>s.
+
+For detailed documentation and examples, visit [https://github.com/ezYZ/lucid]
+
+***
+
+}
